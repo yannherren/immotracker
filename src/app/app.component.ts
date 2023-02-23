f8b40f794ed4479f560ba0bc5e21c6055f4966bb
yhe112114 <hycmedia@icloud.com>
2023-02-23 21:29:11 +0100

    Change style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {environment} from "../environment";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  version = environment.appVersion;
   title = 'immotracker';
 
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -38,7 +38,7 @@ export class DashboardComponent implements OnInit {
   ]
 
   colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+    domain: ['#ED6663', '#B52B65', '#CFC0BB', '#7aa3e5', '#3C2C3E', '#59405C']
   };
 
   roomOptions: number[] = []
