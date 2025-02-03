cc8336fa50e5866245207be558fb5f8c5bd8a35b
Herren Yann Cédric <yann.herren@students.bfh.ch>
2025-02-03 18:03:09 +0100

    update public api key

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   appVersion: 'v1.0.1',
   supabase: {
-    url: 'https://yognrwggtngvhbmbwiuf.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvZ25yd2dndG5ndmhibWJ3aXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcwOTAzMTMsImV4cCI6MTk5MjY2NjMxM30.sTfpsy3uFcH4jny2XFGTfJsiFq_f0xzfOcIsQxA4_EM'
+    url: 'https://aptgmcdjdhocepkzbqcp.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwdGdtY2RqZGhvY2Vwa3picWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1OTU4NTYsImV4cCI6MjA1NDE3MTg1Nn0.0z_tbapf5V0qF9gjsQiVYtz3hcWPb1jPZl7b-cvxcJk'
   }
 }
