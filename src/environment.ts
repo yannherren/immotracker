bd8594648af1b581cb094135cbd997cf70675462
Herren Yann Cédric <yann.herren@students.bfh.ch>
2025-02-03 20:33:11 +0100

    change colors and adjust design

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  appVersion: 'v1.0.1',
+  appVersion: 'v1.2',
   supabase: {
     url: 'https://aptgmcdjdhocepkzbqcp.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwdGdtY2RqZGhvY2Vwa3picWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1OTU4NTYsImV4cCI6MjA1NDE3MTg1Nn0.0z_tbapf5V0qF9gjsQiVYtz3hcWPb1jPZl7b-cvxcJk'
